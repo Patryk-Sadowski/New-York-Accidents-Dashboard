a553baf3dc347b60d57d9bc3559ffcd3f3bd2d80
Patryk-Sadowski <patryksadowski13@gmail.com>
2024-08-28 11:13:28 +0200

    Update NY Accidents.sql

--- NY Accidents.sql
+++ NY Accidents.sql
@@ -13,11 +13,11 @@ order by 4 desc
 
 -- Number of accidents in 3 variants: day and hour, day,  hour only
 select 
-	 distinct dayname(CRASH_DATE) as day_of_week
-	,CRASH_TIME 		as hours
-	,count(*) over (partition by dayname(CRASH_DATE), CRASH_TIME) 	as nr_of_accidents
-	,count(*) over (partition by dayname(CRASH_DATE)) 		as nr_of_accidents_per_day
-	,count(*) over (partition by CRASH_TIME ) 			as nr_of_accidents_per_hour
+	 distinct dayname(CRASH_DATE) 						as day_of_week
+	,CRASH_TIME 								as hours
+	,count(*) over (partition by dayname(CRASH_DATE), CRASH_TIME) 		as nr_of_accidents
+	,count(*) over (partition by dayname(CRASH_DATE)) 			as nr_of_accidents_per_day
+	,count(*) over (partition by CRASH_TIME ) 				as nr_of_accidents_per_hour
 from accidents 
 order by 3 desc
 
@@ -25,8 +25,8 @@ order by 3 desc
 
 select 
 	ON_STREET_NAME  											  
-	,count(*) 							as nr_of_accidents
-	,round((count(*) / (select count(*) from accidents)), 3) *100 	as accidents_per_street_percent
+	,count(*) 								as nr_of_accidents
+	,round((count(*) / (select count(*) from accidents)), 3) *100 		as accidents_per_street_percent
 from accidents 
 group by 1
 order by 3 desc
@@ -37,9 +37,23 @@ select
 	 CONTRIBUTING_FACTOR_VEHICLE_1 		as casue_of_accident
 	,count(*)				as nr_of_accidents
 	,NUMBER_OF_PERSONS_KILLED 
-	,NUMBER_OF_PEDESTRIANS_KILLED 
-	,NUMBER_OF_CYCLIST_KILLED 
-	,NUMBER_OF_MOTORIST_KILLED 
 from accidents
-group by 1, 3, 4, 5, 6
+group by 1, 3
 order by 2 desc
+
+/* Number of victims by district, divided into injured and killed */
+
+select
+	BOROUGH   
+	,case
+		when NUMBER_OF_PEDESTRIANS_INJURED <> 0 then 'Pedestrian Injured'
+		when NUMBER_OF_PEDESTRIANS_KILLED <> 0 then 'Pedestrian Killed'
+		when NUMBER_OF_CYCLIST_INJURED <> 0 then 'Cyclist Injured'
+		when NUMBER_OF_CYCLIST_KILLED <> 0 then 'Cyclist Killed'
+		when NUMBER_OF_MOTORIST_INJURED <> 0 then 'Motorist Injured'
+		when NUMBER_OF_MOTORIST_KILLED <> 0 then 'Motorist Killed'
+		else 'No victim'
+	end as type_of_victim
+	,count(*)  as nr_of_victim
+from accidents 
+group by 1, 2
